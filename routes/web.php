b0cc085b39d6e9f27d17b221302d53acad2472f0
Arkzs <emmanuelcleto.francisco@tup.edu.ph>
2023-06-27 09:26:13 +0800

    emmancommit

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 // MIDDLEWARES
-
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'role:user'])->name('dashboard');
